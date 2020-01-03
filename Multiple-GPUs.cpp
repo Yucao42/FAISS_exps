c3b1d90dc9b1a2c67c46e441f1fe1e4ff95209bc
Yucao42 <yc3390@nyu.edu>
2020-01-03 16:57:35 -0500

    testing on sift 1M

--- Multiple-GPUs.cpp
+++ Multiple-GPUs.cpp
@@ -13,14 +13,19 @@
          << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() \
          << " us" << std::endl; \
 
-#define CPU_KNN_TEST 0     
-#define GPU_KNN_TEST 0
+#define CPU_KNN_TEST 1     
+#define GPU_KNN_TEST 1
 #define CPU_ANN_TEST 0     
-#define GPU_ANN_TEST 1
+#define GPU_ANN_TEST 0
+
 #include <cstdio>
 #include <cstdlib>
 #include <chrono>
 #include <iostream>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 #include <faiss/IndexFlat.h>
 #include <faiss/IndexIVFPQ.h>
@@ -34,35 +39,71 @@
 #include <faiss/gpu/utils/DeviceUtils.h>
 
 
+float * fvecs_read (const char *fname,
+                    size_t *d_out, size_t *n_out)
+{
+    FILE *f = fopen(fname, "r");
+    if(!f) {
+        fprintf(stderr, "could not open %s\n", fname);
+        perror("");
+        abort();
+    }
+    int d;
+    fread(&d, 1, sizeof(int), f);
+    assert((d > 0 && d < 1000000) || !"unreasonable dimension");
+    fseek(f, 0, SEEK_SET);
+    struct stat st;
+    fstat(fileno(f), &st);
+    size_t sz = st.st_size;
+    assert(sz % ((d + 1) * 4) == 0 || !"weird file size");
+    size_t n = sz / ((d + 1) * 4);
+
+    *d_out = d; *n_out = n;
+    float *x = new float[n * (d + 1)];
+    size_t nr = fread(x, sizeof(float), n * (d + 1), f);
+    assert(nr == n * (d + 1) || !"could not read whole file");
+
+    // shift array to remove row headers
+    for(size_t i = 0; i < n; i++)
+        memmove(x + i * d, x + 1 + i * (d + 1), d * sizeof(*x));
+
+    fclose(f);
+    return x;
+}
+
+// not very clean, but works as long as sizeof(int) == sizeof(float)
+int *ivecs_read(const char *fname, size_t *d_out, size_t *n_out)
+{
+    return (int*)fvecs_read(fname, d_out, n_out);
+}
+
+
 int main() {
-// GPU
-    int d = 64;                            // dimension
-    printf("Size of long: %d\n", sizeof(long));
-    long nb = 200 * 1000* 1000;                       // database size
-    int nq = 1000;                        // nb of queries
-    int nlist =  100000;
-    int m = 8;                             // bytes per vector
+    // Basic parameters
+    size_t d = 128;                        // feature dimension
+    int nlist =  100000;                   // number of centroids in coarse quantizer
+    int m = 8;                             // sub-quantizer
+    int k = 10;                            // k-NN k.
 
-    float *xb = new float[d * nb];
-    float *xq = new float[d * nq];
+    // Load sift 1M Data
+    // Training data
+    size_t nt;
+    float *xt = fvecs_read("sift/sift_learn.fvecs", &d, &nt);
 
-    for(long i = 0; i < nb; i++) {
-        for(long j = 0; j < d; j++)
-            xb[d * i + j] = drand48();
-        xb[d * i] += i / 1000.;
-    }
+    // Database data
+    size_t nb, d2;
+    float *xb = fvecs_read("sift/sift_base.fvecs", &d2, &nb);
+    assert(d == d2 || !"dataset does not have same dimension as train set");
 
-    for(int i = 0; i < nq; i++) {
-        for(int j = 0; j < d; j++)
-            xq[d * i + j] = drand48();
-        xq[d * i] += i / 1000.;
-    }
+    // Query data
+    size_t d3, nq;
+    float *xq = fvecs_read("sift/sift_query.fvecs", &d3, &nq);
+    assert(d == d3 || !"query does not have same dimension as train set");
 
     auto start = std::chrono::steady_clock::now();
     auto end = std::chrono::steady_clock::now();
     int ngpus = faiss::gpu::getNumDevices();
     printf("Number of GPUs: %d\n", ngpus);
-    int k = 4;
 
     std::vector<faiss::gpu::GpuResources*> res;
     std::vector<int> devs;
@@ -70,12 +111,20 @@ int main() {
         res.push_back(new faiss::gpu::StandardGpuResources);
         devs.push_back(i);
     }
+
+    // If sharded or replicated in multiple GPUs.
     faiss::gpu::GpuMultipleClonerOptions option;
     option.shard = true;
+
+
+    for(int i = 0; i < 128; i++)
+      std::cout << xb[i] << std::endl;
 
 #if GPU_KNN_TEST >0
     faiss::IndexFlatL2 cpu_index(d);
 
+    printf("migrating index from cpu to gpu");
+    start = std::chrono::steady_clock::now();
     faiss::Index *gpu_index =
         faiss::gpu::index_cpu_to_gpu_multiple(
             res,
@@ -83,6 +132,10 @@ int main() {
             &cpu_index,
             &option
         );
+    end = std::chrono::steady_clock::now();
+    std::cout << "[Migrating time] "
+         << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+         << " us" << std::endl;
 
     printf("is_trained = %s\n", gpu_index->is_trained ? "true" : "false");
     start = std::chrono::steady_clock::now();
@@ -93,6 +146,35 @@ int main() {
          << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
          << " us" << std::endl;
 
+    {       // sanity check: search 5 first vectors of xb
+        long *I = new long[k * 5];
+        float *D = new float[k * 5];
+
+        start = std::chrono::steady_clock::now();
+        gpu_index->search(5, xb, k, D, I);
+        end = std::chrono::steady_clock::now();
+        std::cout << "[SEARCH TIME] [GPU BF] search " << 5 << " records in microseconds : "
+             << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
+             << " us" << std::endl;
+
+        // print results
+        printf("I=\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < k; j++)
+                printf("%5ld ", I[i * k + j]);
+            printf("\n");
+        }
+
+        printf("D=\n");
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < k; j++)
+                printf("%7g ", D[i * k + j]);
+            printf("\n");
+        }
+
+        delete [] I;
+        delete [] D;
+    }
 
     {       // search xq
         long *I = new long[k * nq];
@@ -177,7 +259,7 @@ int main() {
         index.search(nq, xq, k, D, I);
         auto end = std::chrono::steady_clock::now();
         printf("ntotal = %ld\n", gpu_index->ntotal);
-        std::cout << "[SEARCH TIME] [GPU BF]Search time for queries " << 1000 << " records in microseconds : "
+        std::cout << "[SEARCH TIME] [CPU BF]Search time for queries " << nq << " records in microseconds : "
          << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count()
          << " us" << std::endl;
 
